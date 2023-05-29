ff61161feef674f5ebf965c4ca15b5ad33449c57
Arkadiusz Chrabąszczewski <Arkadiuszchr@gmail.com>
2023-05-29 13:59:07 +0200

    fix: prettier formatting

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
-import { dangerReassure } from 'reassure';
+import {dangerReassure} from 'reassure';
 
 dangerReassure({
-  inputFilePath: path.join(__dirname, './.reassure/output.md'),
+    inputFilePath: path.join(__dirname, './.reassure/output.md'),
 });
